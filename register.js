0f66e3ccdb64aeac298d0956b39618e3a1c21c26
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2023-10-04 21:24:05 +1030

    Add files via upload

---
+++ register.js
@@ -0,0 +1,6 @@
+function register()
+{
+    document.getElementById('registerSubmitButton').onclick = function() {alert("button was clicked1");}
+}
+
+register();
---
+++ signin.js
@@ -0,0 +1,7 @@
+function login()
+{
+    document.getElementById('loginSubmitButton').onclick = function() {alert("button was clicked");}
+    document.getElementById('registerSubmitButton').onclick = function() {alert("button was clicked1");}
+}
+
+login();
