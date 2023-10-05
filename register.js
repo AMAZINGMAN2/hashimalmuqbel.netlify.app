4d8e495f28aa81d7dc2661b215f2455dcd721ef9
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2023-10-05 20:01:50 +1030

    Update register.js

--- register.js
+++ register.js
@@ -1,6 +1,33 @@
-function register()
-{
-    document.getElementById('registerSubmitButton').onclick = function() {alert("button was clicked1");}
-}
+var username, password, email;
 
-register();
+window.addEventListener('load', function() {
+    // Get the input values when the 'load' event occurs
+    username = document.getElementById('username1').value;
+    password = document.getElementById('password-input').value;
+    email = document.getElementById('email').value;
+});
+
+document.getElementById('registerSubmitButton').onclick = function() {
+    // Get the input values when the 'registerSubmitButton' is clicked
+    username = document.getElementById('username1').value;
+    password = document.getElementById('password-input').value;
+    email = document.getElementById('email').value;
+    registerwithemail();
+};
+
+function registerwithemail() {
+    var loginWithPlayfabRequest = {
+        TitleId: "B58F6",
+        Username: username,
+        Password: password,
+        Email: email
+    };
+
+    PlayFabClientSDK.RegisterPlayFabUser(loginWithPlayfabRequest, function (result, error) {
+        if (result !== null) {
+            console.log('Register successful:', result);
+        } else if (error !== null) {
+            console.error('Register error:', error);
+        }
+    });
+}
