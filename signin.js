f63f988a740e98fdce5d32d17a1ba46bfcac2801
Hashim Almuqbel <85612244+AMAZINGMAN2@users.noreply.github.com>
2023-10-05 18:02:11 +1030

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,7 +1,56 @@
+
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?`~';
+
+function generateString(length) {
+    let result = ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+}
+
+
+function LoginWithCustomID(){
+    PlayFab.settings.titleId = "B58F6";
+    var loginRequest = {
+        TitleId: PlayFab.settings.titleId,
+        CustomId: generateString(16),
+        CreateAccount: false
+    };
+
+    PlayFabClientSDK.LoginWithCustomID(loginRequest, LoginCallback);
+}
+
+var LoginCallback = function (result, error) {
+    if (result !== null) {
+        document.getElementById("resultOutput").innerHTML = "Congratulations, you made your first successful API call!";
+    } else if (error !== null) {
+        document.getElementById("resultOutput").innerHTML =
+            "Something went wrong with your first API call.\n" +
+            "Here's some debug information:\n" +
+            CompileErrorReport(error);
+    }
+}
+
+// This is a utility function we haven't put into the core SDK yet.  Feel free to use it.
+function CompileErrorReport(error) {
+    if (error === null)
+        return "";
+    var fullErrors = error.errorMessage;
+    for (var paramName in error.errorDetails)
+        for (var msgIdx in error.errorDetails[paramName])
+            fullErrors += "\n" + paramName + ": " + error.errorDetails[paramName][msgIdx];
+    return fullErrors;
+}
 function login()
 {
-    document.getElementById('loginSubmitButton').onclick = function() {alert("button was clicked");}
-    document.getElementById('registerSubmitButton').onclick = function() {alert("button was clicked1");}
+    document.getElementById('loginSubmitButton').onclick = function()
+    {
+        LoginWithCustomID();
+        alert("should call the function")
+    }
 }
 
 login();
