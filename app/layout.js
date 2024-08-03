400750b940e694b23fffa28788cd6d3d0c52856f
AMAZINGMAN2 <hashim.almuqbel@gmail.com>
2024-08-04 00:33:51 +0300

    4/8/2024 fixed layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export const metadata = {
   title: "Hashim Almuqbel - Portfolio", 
-  description: "This is Hashim Almuqbel's Portfolio",
+  description: "This is Hashim Almuqbels Portfolio",
 };
 
 export default function RootLayout({ children }) {
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
           />
         </div>
         <main className="min-h-screen flex flex-col items-center justify-between relative z-10 ">
-        <h1 className="text-5xl font-bold leading-normal -ml-[47vw] mt-[20vh] z-25 "><span className="bg-gradient-to-r from-blue-600 via-violet-500 to-green-500 text-transparent bg-clip-text">Empowering</span> Tomorrow&apos;s <br/> <span className="bg-gradient-to-r from-pink-600 via-violet-700 to-purple-800 text-transparent bg-clip-text">Innovations</span> Through <span className="bg-gradient-to-br from-blue-600 via-purple-500 to-pink-500 text-transparent bg-clip-text">Creativity!</span></h1>
+        <h1 className="text-5xl font-bold leading-normal -ml-[47vw] mt-[20vh] z-25 "><span className="bg-gradient-to-r from-blue-600 via-violet-500 to-green-500 text-transparent bg-clip-text">Empowering</span> Tomorrows <br/> <span className="bg-gradient-to-r from-pink-600 via-violet-700 to-purple-800 text-transparent bg-clip-text">Innovations</span> Through <span className="bg-gradient-to-br from-blue-600 via-purple-500 to-pink-500 text-transparent bg-clip-text">Creativity!</span></h1>
           <CodeAnimator />
           {children}
         </main>
