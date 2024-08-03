a02ee13c460fdd37a80fec4b67ebe5f739f5a621
AMAZINGMAN2 <hashim.almuqbel@gmail.com>
2024-08-04 00:24:48 +0300

    Revert "removed eslint"
    This reverts commit db23fabbe91ae3dd81a757f60cfe44ee999362c0.

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-      ignoreDuringBuilds: true, // Ignore ESLint errors during production builds
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
     },
   };
   
