a2dba7e277d2762f1e40173a1db9af76e1457fa5
AMAZINGMAN2 <hashim.almuqbel@gmail.com>
2024-08-04 20:09:20 +0300

    4/8/24 fixed eslintrc.json erors

--- app/hero.js
+++ app/hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
         <h2 className="text-sm text-purple-400 mx-auto justify-center flex tracking-wide uppercase">Hello,<br/></h2>
         <h1 className="text-4xl lg:text-6xl font-black mt-2">
         <br/>
-          I'm Hashim Almuqbel.
+          I&apos;m Hashim Almuqbel.
         </h1>
         <p className="mt-4 text-lg font-thin lg:text-xl max-w-2xl mx-auto lg:mx-0">
           I like Programming & Mathematics, thats why i decided to make various programming projects involving complex algorithms and tricks, I enjoy creating and optimising Machine learning models aswell as making proffessional looking websites like this! Here are some projects that I have made:
